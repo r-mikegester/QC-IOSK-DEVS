3aa49fa56bd4c0688fe4122b202b7e3ee89d222e
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-07 05:37:29 +0800

    Added QCU Logo

--- src/pages/components/Welcome.tsx
+++ src/pages/components/Welcome.tsx
@@ -1,7 +1,6 @@
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
-import ExploreContainer from './ExploreContainer';
+import { IonPage, IonContent } from '@ionic/react';
 import './ExploreContainer.css';
-
+import QCULogo from '../../imgs/Logo/cropped-qculogopng-1-copy-300x300.png';
 interface ContainerProps {
   name: string;
 }
@@ -9,18 +8,20 @@ interface ContainerProps {
 const CampusSelect: React.FC<ContainerProps> = ({ name }) => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>CAMPUS INA</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">SELECT CAMPUS</IonTitle>
-          </IonToolbar> 
-        </IonHeader>
-        <ExploreContainer name="SELECT CAMPUS" />
+      <section className="flex items-center h-full p-16 dark:text-gray-100">
+	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+		<div className="max-w-md text-center">
+      <img src={QCULogo} className="mx-auto w-40 -mt-80" />
+			<h2 className="mb-8 font-extrabold text-7xl dark:text-gray-600">
+				<span className="bg-clip-text bg-gradient-to-tr from-blue-600 to-purple-400 text-transparent">QCU-IOSK</span>
+			</h2>
+			
+			<a rel="noopener noreferrer" href="/SelectCampus" className="px-8 py-3 font-semibold border-2 mt-20  border-sky-700 rounded-2xl">Continue</a>
+		</div>
+	</div>
+</section>
+
       </IonContent>
     </IonPage>
   );
