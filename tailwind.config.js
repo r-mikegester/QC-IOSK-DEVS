c2d70791ca9a4e80927d0c56bdd2ee9c4984eb92
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-08 06:43:54 +0800

    added optional config about plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // plugins(({ theme, addUtilities}) => {
+    //   const neonUtilities = {};
+    //   const colors = theme('colors');
+    //   for (const color in colors){
+    //     if (typeof colors[color] === 'object'){
+    //       const color1 = colors[color]['500'];
+    //       const color2 = colors[color]['700'];
+    //       neonUtilities[`.neon-${color}`] = {
+    //         boxShadow: `0 0 5px ${color}, 0 0 20px ${color2}`
+    //       };
+    //     }
+    //   }
+    //   addUtilities(neonUtilities)
+    // })
+  ],
 }
 
