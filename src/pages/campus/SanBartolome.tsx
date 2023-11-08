a77dc2e78f399521931a8364599e54c5d91f82b8
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-08 17:50:51 +0800

    Updated og file and imported sky

--- src/pages/campus/SanBartolome.tsx
+++ src/pages/campus/SanBartolome.tsx
@@ -1,6 +1,6 @@
 import { Canvas, useThree } from "@react-three/fiber";
 import React, { useRef } from 'react';
-import { PresentationControls, OrbitControls, useGLTF, Stage } from "@react-three/drei";
+import { PresentationControls, OrbitControls, useGLTF, Stage, Sky } from "@react-three/drei";
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 
@@ -9,17 +9,17 @@ interface ModelProps {
 }
 
 function Model(props: ModelProps) {
-  const { scene } = useGLTF("/og.glb");
-  
+  const { scene } = useGLTF("/og_v3.glb");
+
   return <primitive object={scene} {...props} />
 }
 
 const Controls: React.FC = () => {
- const controls = useRef<typeof OrbitControls>();
+  const controls = useRef<typeof OrbitControls>();
   const { camera, gl } = useThree();
 
   return (
-    <PresentationControls speed={1.5} global zoom={0.2} polar={[-0.10, Math.PI / 4]}>
+    <PresentationControls speed={1.5} global zoom={10} polar={[-0.10, Math.PI / 4]}>
       <OrbitControls autoRotate args={[camera, gl.domElement]} />
     </PresentationControls>
   );
@@ -27,12 +27,19 @@ const Controls: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <Canvas dpr={[1, 2]} shadows camera={{ fov: 75 }} className="bg-gradient-to-tr from-blue-600 to-purple-400" style={{"position": "absolute"}}>
+    <Canvas dpr={[1, 2]} shadows camera={{ fov: 75 }} position={[10,20,20]} className="bg-gradient-to-tr from-blue-600 to-purple-400" style={{ "position": "absolute" }}>
       {/* <color attach="background"  args={["#64abbf"]} /> */}
-      <Controls /> 
-      <Stage environment={"warehouse"}>
+      <Controls />
+      <Stage environment={"warehouse"} adjustCamera>
         <Model scale={0.01} />
       </Stage>
+      <Sky
+        distance={450000} // Camera distance (default=450000)
+        sunPosition={[0, 20, 0]} // Sun position normal (defaults to inclination and azimuth if not set)
+        inclination={1} // Sun elevation angle from 0 to 1 (default=0)
+        azimuth={1} // Sun rotation around the Y axis from 0 to 1 (default=0.25)
+       
+      />
     </Canvas>
   );
 }
