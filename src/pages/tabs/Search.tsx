2858213910e25736f7bdaf793caddb18d87617a2
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-08 18:07:19 +0800

    Renamed for to htmlFor

--- src/pages/tabs/Search.tsx
+++ src/pages/tabs/Search.tsx
@@ -23,7 +23,7 @@ const Search: React.FC = () => {
                 <form>
                   <div className="relative z-10 flex space-x-3 p-3 bg-white border rounded-2xl shadow-lg shadow-gray-100 dark:bg-slate-300 dark:border-gray-400 dark:shadow-gray-900/[.2]">
                     <div className="flex-[1_0_0%]">
-                      <label for="hs-search-article-1" className="block text-sm text-gray-700 font-medium dark:text-white"><span className="sr-only">Search...</span></label>
+                      <label htmlFor="hs-search-article-1" className="block text-sm text-gray-700 font-medium dark:text-white"><span className="sr-only">Search...</span></label>
                       <input type="email" name="hs-search-article-1" id="hs-search-article-1" className="py-2.5 px-4 block w-full border-transparent rounded-2xl focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-200 dark:border-transparent dark:text-gray-400 dark:focus:ring-emerald-200" placeholder="Search" />
                     </div>
                     <div className="flex-[0_0_auto]">
