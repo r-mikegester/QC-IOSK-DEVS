e91a49f8663fe49b9d84b9602022bf8fe9bb6943
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-13 21:01:37 +0800

    removed unused props, and css files

--- src/pages/tabs/Map.tsx
+++ src/pages/tabs/Map.tsx
@@ -1,16 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import SanBartolome from '../campus/SanBartolome';
 import Dock from '../components/dock';
-import './Map.css';
-interface ContainerProps {
-  name: string;
-}
-const Map: React.FC<ContainerProps> = ({ name }) => {
+
+interface ContainerProps {}
+
+const Map: React.FC<ContainerProps> = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
         <SanBartolome />
-        <Dock name={}/>
+        <Dock />
       </IonContent>
     </IonPage>
   );
