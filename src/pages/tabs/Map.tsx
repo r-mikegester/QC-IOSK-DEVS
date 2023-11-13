b68858a3677c2a81122eacec43837b1624bc6e1a
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-13 21:05:12 +0800

    removed unused external container props

--- src/pages/tabs/Map.tsx
+++ src/pages/tabs/Map.tsx
@@ -2,9 +2,8 @@ import { IonContent, IonPage } from '@ionic/react';
 import SanBartolome from '../campus/SanBartolome';
 import Dock from '../components/dock';
 
-interface ContainerProps {}
 
-const Map: React.FC<ContainerProps> = () => {
+const Map: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
