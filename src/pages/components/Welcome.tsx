32f462189382908093ebef89e774868a60711042
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-13 21:52:44 +0800

    fix landing page layout and display

--- src/pages/components/Welcome.tsx
+++ src/pages/components/Welcome.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IonPage, IonContent } from '@ionic/react';
-import QCULogo from '../../imgs/Logo/cropped-qculogopng-1-copy-300x300.png';
+import QCULogo from '../../imgs/Logo/qculogo.png';
 import Sample from '../../vids/sample4.mp4';
 import Samplecp from '../../vids/samplecp.mp4';
 import { useHistory } from 'react-router-dom';
@@ -14,7 +14,7 @@ const CampusSelect: React.FC<ContainerProps> = ({ name }) => {
 
   const handleClick = () => {
     // Redirect to the "/Map" route
-    history.push('/Map');
+    history.push('/SelectCampus');
   };
 
   return (
@@ -25,12 +25,12 @@ const CampusSelect: React.FC<ContainerProps> = ({ name }) => {
           <video src={Samplecp} autoPlay muted loop className="h-screen md:hidden"></video>
           <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
             <div className="max-w-md text-center">
-              <img src={QCULogo} className="mx-auto w-40 -mt-80" />
+              <img src={QCULogo} className="mx-auto absolute inset-x-0 top-96 w-40 -mt-80" alt="QCU Logo" />
               <h2 className="mb- font-extrabold text-7xl dark:text-gray-600">
-                <span className="bg-clip-text bg-gradient-to-tr drop-shadow-md text-5xl from-blue-600 to-purple-400 text-transparent">QCU-IOSK</span>
+                <span className="absolute inset-x-0 top-64 bg-clip-text bg-gradient-to-tr drop-shadow-md text-5xl from-blue-600 to-purple-400 text-transparent">QCU-IOSK</span>
               </h2>
               {/* Use onClick to handle the redirection */}
-              <div  className=" animate-bounce  py-3  drop-shadow-md ">
+              <div  className=" animate-bounce absolute inset-x-0 top-80 py-3  drop-shadow-md ">
                 Touch to<span className="font-extrabold text-lg"> Start</span>
               </div>
             </div>
