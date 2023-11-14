00c09d14040eef8f59cc060d18ce3f2d8c96da57
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-14 18:12:09 +0800

    added modal, fix orbital

--- src/pages/campus/SanBartolome.tsx
+++ src/pages/campus/SanBartolome.tsx
@@ -1,72 +1,50 @@
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import React, { useRef, useState, Suspense} from 'react';
-import { PresentationControls, OrbitControls, useGLTF, Stage, Sky, meshBounds, Bounds } from "@react-three/drei";
-//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-//import { Selection, Select, EffectComposer, Outline } from '@react-three/postprocessing';
+import { Canvas, useLoader } from "@react-three/fiber";
+import React, { Suspense } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
-
-async function loadBuilding() {
-  const loader = new GLTFLoader();
-  }
-  
-  export { loadBuilding };
+import { OrbitControls, Stage, Sky } from "@react-three/drei";
 
 interface ModelProps {
+  url: string;
   scale: number;
 }
 
 function Model(props: ModelProps) {
-  const ref = useRef()
-  const { scene } = useGLTF("/og_v4.glb");
-  const [hovered, hover] = useState(null)
-  console.log(hovered)
-  // useFrame((state: any, delta: any) => (ref.current.rotation.x = ref.current.rotation.y += delta))
-   return <primitive object={scene} {...props} />
-
-  
-}
-interface ContainerProps {
-  name: string;
+  const gltf = useLoader(GLTFLoader, props.url);
+  const handleClick = () => {
+    console.log("Building clicked!");
+    document.getElementById('SelectBuilding').showModal()
+  };
+  return <primitive object={gltf.scene} onClick={handleClick} scale={[props.scale, props.scale, props.scale]} />;
 }
 
-const App: React.FC<ContainerProps> = ({ name }) => {
+const App: React.FC = () => {
   return (
-    
-    <Canvas orthographic dpr={[1, 2]} shadows camera={{ fov: 75, position: [10,10,10], zoom: 10 }} className="bg-gradient-to-tr from-blue-600 to-purple-400" style={{ "position": "absolute" }}>
-      {/* <color attach="background"  args={["#64abbf"]} /> */}
+    <Canvas orthographic dpr={[1, 2]} speed={0.1} shadows camera={{ fov: 75, position: [10, 10, 10], zoom: 10 }} className="bg-gradient-to-tr from-blue-600 to-purple-400" style={{ "position": "absolute" }}>
       <OrbitControls
         makeDefault
         minAzimuthAngle={0}
-        maxAzimuthAngle={Math.PI / 2}
+        maxAzimuthAngle={Math.PI / 0}
         minPolarAngle={Math.PI / 3}
-        maxPolarAngle={Math.PI / 3}
+        maxPolarAngle={Math.PI / 6}
         enableZoom={true}
         enablePan={true}
         zoomSpeed={0.3}
+        autoRotate
       />
-       {/* <gridHelper args={[1000, 200, '#151515', '#020202']} position={[0, -1, 0]} /> */}
       <Suspense fallback={null}>
-  {/* <Selection> */}
-  <mesh onClick={(e) => console.log('click')} raycast={meshBounds} >
-    
-
-       <Stage environment={"warehouse"} adjustCamera>
-         <Model scale={0.01} />
-       
-        
-       </Stage>
-       <Sky
-         distance={450000} // Camera distance (default=450000)
-         sunPosition={[0, 20, 0]} // Sun position normal (defaults to inclination and azimuth if not set)
-         inclination={1} // Sun elevation angle from 0 to 1 (default=0)
-         azimuth={1} // Sun rotation around the Y axis from 0 to 1 (default=0.25)
-        
-       />
-    </mesh>
-  {/* </Selection> */}
+        <Stage environment={"warehouse"} adjustCamera>
+          <Model url="/og_v4.glb" scale={0.01} position={1,0,0}/>
+          <Model url="/og_v4.glb" scale={0.01} position={1,1,0}/>
+        </Stage>
+        <Sky
+          distance={450000}
+          sunPosition={[0, 20, 0]}
+          inclination={1}
+          azimuth={1}
+        />
       </Suspense>
     </Canvas>
+    
   );
 }
 
--- src/pages/tabs/Map.tsx
+++ src/pages/tabs/Map.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonPage } from '@ionic/react';
 import SanBartolome from '../campus/SanBartolome';
 import Dock from '../components/dock';
+import Controls from '../components/controls';
 
 interface ContainerProps {
   name: string;
@@ -9,7 +10,29 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <SanBartolome name={'SanBartolome'}/>
+        <SanBartolome name={'SanBartolome'} />
+        <div className="absolute bottom-10 right-10 ">
+          <dialog id="SelectBuilding" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Select Building</h3>
+              <div className="grid grid-cols-4">
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">1</div>
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">2</div>
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">3</div>
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">4</div>
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">5</div>
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">6</div>
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">7</div>
+                  <div className="bg-red-500 w-20 h-20 m-2 rounded-2xl">8</div>
+              </div>
+
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+          <Controls name={'Controls'} />
+        </div>
         <Dock name={'Dock'} />
       </IonContent>
     </IonPage>
