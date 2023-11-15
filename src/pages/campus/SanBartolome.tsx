00394b8618369530d01d1593b8041de4a295b38b
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-15 18:25:27 +0800

    added a handler that clicks

--- src/pages/campus/SanBartolome.tsx
+++ src/pages/campus/SanBartolome.tsx
@@ -1,25 +1,32 @@
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas, useLoader, extend } from "@react-three/fiber";
 import React, { Suspense } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls, Stage, Sky, Gltf } from "@react-three/drei";
-
 interface ModelProps {
   url: string;
   scale: number;
+  name: string;
+}
+interface ContainerProps {
+  name: string;
 }
 
 function Model(props: ModelProps) {
   const gltf = useLoader(GLTFLoader, props.url);
   const handleClick = () => {
     console.log("Building clicked!");
-    document.getElementById('SelectBuilding').showModal()
+    const element = document.getElementById('SelectBuilding');
+    if (element) {
+      //element.showModal();
+    }
   };
   return <primitive object={gltf.scene} onClick={handleClick} scale={[props.scale, props.scale, props.scale]} />;
 }
 
-const App: React.FC = () => {
+const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <Canvas orthographic dpr={[1, 2]} speed={0.1}  shadows camera={{ fov: 75, position: [10, 10, 10], zoom: 10 }} className="bg-gradient-to-tr from-blue-600 to-purple-400" style={{ "position": "absolute" }}>
+    <Canvas frameloop="demand" orthographic dpr={[1, 2]} shadows camera={{ fov: 75, position: [10, 10, 10], zoom: 10 }} className="bg-gradient-to-tr from-blue-600 to-purple-400" style={{ "position": "absolute" }}>
+      
       <OrbitControls
         makeDefault
         minAzimuthAngle={0}
@@ -35,8 +42,7 @@ const App: React.FC = () => {
       />
       <Suspense fallback={null}>
         <Stage environment={"warehouse"} adjustCamera>
-          <Model url="/og_v4.glb"  scale={2} position={1,0,0}/>
-          <Model url="/og_v4.glb" scale={2} position={1,1,0}/>
+          <Model url="/og_v4.glb" scale={2} name={"SanBartolome"}/>
         </Stage>
         <Sky
           distance={450000}
@@ -50,4 +56,4 @@ const App: React.FC = () => {
   );
 }
 
-export default App;
+export default SanBartolome;
--- src/pages/tabs/Map.tsx
+++ src/pages/tabs/Map.tsx
@@ -2,6 +2,7 @@ import { IonContent, IonPage } from '@ionic/react';
 import SanBartolome from '../campus/SanBartolome';
 import Dock from '../components/dock';
 import Controls from '../components/controls';
+import Back from '../components/Backbtn';
 
 interface ContainerProps {
   name: string;
@@ -13,24 +14,26 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
       <div className="absolute top-0 right-20 z-50 bg-red-500">
          <Controls name={'Controls'} />
          </div>
-        <SanBartolome name={'SanBartolome'} />
+      <div className="z-10">
+      <SanBartolome name={'SanBartolome'} />
+      </div>
         <div className="absolute bottom-10 right-10 ">
           <dialog id="SelectBuilding" className="modal ml-96">
             <div className="modal-box max-w-3xl ">
               <h3 className="font-bold text-center text-3xl">Select Building</h3>
               <div className="grid grid-cols-4 gap-1">
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">1</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">2</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">3</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">4</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">5</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">6</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">7</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">8</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">9</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">10</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">11</div>
-                  <div className="bg-red-500 w-40 h-40 m-2 rounded-2xl text-center ">12</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">1</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">2</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">3</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">4</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">5</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">6</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">7</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">8</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">9</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">10</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">11</div>
+                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">12</div>
               </div>
             </div>
             <form method="dialog" className="modal-backdrop">
@@ -40,6 +43,9 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
         
         </div>
         <Dock name={'Dock'} />
+      <div className="z-50">
+      <Back name={'Back'} />
+      </div>
       </IonContent>
     </IonPage>
   );
