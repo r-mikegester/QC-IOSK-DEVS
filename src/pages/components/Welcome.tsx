332a2b442d9ae6a4d2d403480a48bbab275ddb6b
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-15 18:26:35 +0800

    updated the portrait video for mobile

--- src/pages/components/Welcome.tsx
+++ src/pages/components/Welcome.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IonPage, IonContent } from '@ionic/react';
 import QCULogo from '../../imgs/Logo/qculogo.png';
 import Sample from '../../vids/sample4.mp4';
-import Samplecp from '../../vids/samplecp.mp4';
+import Samplecp from '../../vids/samplecp2.mp4';
 import { useHistory } from 'react-router-dom';
 
 interface ContainerProps {
@@ -25,9 +25,9 @@ const CampusSelect: React.FC<ContainerProps> = ({ name }) => {
           <video src={Samplecp} autoPlay muted loop className="h-screen md:hidden"></video>
           <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
             <div className="max-w-md text-center">
-              <img src={QCULogo} className="mx-auto absolute inset-x-0 top-96 w-72 -mt-80" alt="QCU Logo" />
+              <img src={QCULogo} className="mx-auto absolute inset-x-0 top-96 w-40 xl:w-72 -mt-56 xl:-mt-80" alt="QCU Logo" />
               <h2 className="mb- font-extrabold text-7xl dark:text-gray-600">
-                <span className="absolute inset-x-0 top-96 bg-clip-text bg-gradient-to-tr drop-shadow-md text-5xl from-blue-600 to-purple-400 text-transparent">QCU-IOSK</span>
+                <span className="absolute inset-x-0 top-80 mt-3 xl:mt-16 bg-clip-text bg-gradient-to-tr drop-shadow-md text-5xl from-blue-600 to-purple-400 text-transparent">QCU-IOSK</span>
               </h2>
               {/* Use onClick to handle the redirection */}
               <div  className=" animate-bounce absolute inset-x-0 bottom-96 py-3  drop-shadow-md ">
