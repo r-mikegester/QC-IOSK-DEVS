8b27cde135e7968de843a23884d248f333ec0e30
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-17 07:27:46 +0800

    added a modal that display if user click buildings

--- src/pages/tabs/Map.tsx
+++ src/pages/tabs/Map.tsx
@@ -2,7 +2,6 @@ import { IonContent, IonPage } from '@ionic/react';
 import SanBartolome from '../campus/SanBartolome';
 import Dock from '../components/dock';
 import Controls from '../components/controls';
-import Back from '../components/Backbtn';
 
 interface ContainerProps {
   name: string;
@@ -10,42 +9,39 @@ interface ContainerProps {
 const Map: React.FC<ContainerProps> = ({ name }) => {
   return (
     <IonPage>
-      <IonContent fullscreen> 
-      <div className="absolute top-0 right-20 z-50 bg-red-500">
-         <Controls name={'Controls'} />
-         </div>
-      <div className="z-10">
-      <SanBartolome name={'SanBartolome'} />
-      </div>
+      <IonContent fullscreen>
+     
+        <div className="absolute top-0 right-20 z-50 ">
+          <Controls name={'Controls'} />
+        </div>
+        <div className="z-10">
+          <SanBartolome name={'SanBartolome'} />
+        </div>
         <div className="absolute bottom-10 right-10 ">
-          <dialog id="SelectBuilding" className="modal ml-96">
+          <dialog id="SelectBuilding" className="modal">
             <div className="modal-box max-w-3xl ">
-              <h3 className="font-bold text-center text-3xl">Select Building</h3>
+              <h3 className="font-bold text-center text-5xl">{name} Building</h3>
               <div className="grid grid-cols-4 gap-1">
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">1</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">2</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">3</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">4</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">5</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">6</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">7</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">8</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">9</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">10</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">11</div>
-                  <div className="bg-blue-300 w-40 h-40 m-2 rounded-2xl text-center ">12</div>
+                
+              </div>
+              <h3 className="font-bold text-center text-3xl">Select Floor</h3>
+              <div className="grid grid-cols-4 gap-1">
+                <div className="bg-sky-900 w-40 h-40 m-2 rounded-2xl text-center justify-center py-16 text-xl backdrop-blur-lg ">1</div>
+                <div className="bg-sky-900 w-40 h-40 m-2 rounded-2xl text-center justify-center py-16 text-xl backdrop-blur-lg ">2</div>
+                <div className="bg-sky-900 w-40 h-40 m-2 rounded-2xl text-center justify-center py-16 text-xl backdrop-blur-lg ">3</div>
+                <div className="bg-sky-900 w-40 h-40 m-2 rounded-2xl text-center justify-center py-16 text-xl backdrop-blur-lg ">4</div>
+               
               </div>
             </div>
+            
             <form method="dialog" className="modal-backdrop">
               <button>close</button>
             </form>
           </dialog>
-        
+
         </div>
         <Dock name={'Dock'} />
-      <div className="z-50">
-      <Back name={'Back'} />
-      </div>
+
       </IonContent>
     </IonPage>
   );
