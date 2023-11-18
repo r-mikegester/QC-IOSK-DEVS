e17ec685098b776e86163d60d672d00051285e40
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-18 22:34:10 +0800

    renamed to qc-iosk

--- src/pages/components/Welcome.tsx
+++ src/pages/components/Welcome.tsx
@@ -27,7 +27,7 @@ const CampusSelect: React.FC<ContainerProps> = ({ name }) => {
             <div className="max-w-md text-center">
               <img src={QCULogo} className="mx-auto absolute inset-x-0 top-96 w-40 sm:w-32 md:w-48 lg:w-64 xl:w-72 -mt-56 xl:-mt-80" alt="QCU Logo" />
               <h2 className="mb- font-extrabold text-7xl dark:text-gray-600">
-                <span className="absolute inset-x-0 top-80 mt-3 xl:mt-16 bg-clip-text bg-gradient-to-tr drop-shadow-md text-5xl from-blue-600 to-purple-400 text-transparent">QC-IOSK</span>
+                <span className="absolute inset-x-0 top-80 mt-3 xl:mt-16 bg-clip-text bg-gradient-to-tr drop-shadow-md text-5xl text-white">QC-IOSK</span>
               </h2>
               {/* Use onClick to handle the redirection */}
               <div  className=" animate-bounce absolute inset-x-0 bottom-96 py-3  drop-shadow-md ">
