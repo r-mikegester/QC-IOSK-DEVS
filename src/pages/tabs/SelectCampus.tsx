5906fdd0b57fbf062b602c1135caa28d701c6703
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-24 07:13:20 +0800

    refreshed the design of the select campus

--- src/pages/tabs/SelectCampus.tsx
+++ src/pages/tabs/SelectCampus.tsx
@@ -4,10 +4,13 @@ import SBCampus from '../../imgs/SelectCampus/SanBartolome.webp';
 import BatasanCampus from '../../imgs/SelectCampus/Batasan.webp';
 import SFCampus from '../../imgs/SelectCampus/SanFransisco.webp';
 //import SanBartolome from '../campus/SanBartolome';
-import SanFransisco from './../campus/SanFransisco';
-import Batasan from './../campus/Batasan';
+import SanFransisco from '../components/campus/SanFransisco';
+import Batasan from '../components/campus/Batasan';
 import Dock from './../components/dock';
+import QCULogo from '../../imgs/Logo/qculogo.png';
+import { Icon } from '@iconify/react';
 import { useHistory } from 'react-router-dom';
+import './style.css';
 interface ContainerProps {
   name: string;
 }
@@ -28,125 +31,125 @@ const SelectCampus: React.FC<ContainerProps> = ({ name }) => {
   };
   return (
     <IonPage>
-      <IonContent fullscreen className="">
-        <div className="max-w-[85rem] bg-white px-4 h-full py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <IonContent fullscreen className="bg-sc">
+      <div className="max-w-[85rem] px-4 h-full py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
           <div className="max-w-2xl mx-auto text-center mb-10">
-          <h2 className="mt-1 sm:mt-3 text-4xl font-bold text-white sm:text-6xl">
-              <span className="bg-clip-text bg-gradient-to-tr from-blue-600 to-purple-400 text-transparent">Change Campus </span>
+            <img src={QCULogo} className="mx-auto absolute inset-x-0 top-80 w-28 sm:w-32 md:w-48 lg:w-56 xl:w-36 xl:h-36 -mt-56 xl:-mt-80" alt="QCU Logo" />
+            <h2 className="mt-44 sm:mt-3 xl:mt-48 text-4xl font-light text-gray-100  sm:text-6xl">
+              <span className="">Select Campus </span>
             </h2>
-            
+
           </div>
 
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-6">
-            <a className="group rounded-2xl overflow-hidden hidden md:block" onClick={ClickSB}>
-              <div className="relative pt-[50%] sm:pt-[70%] rounded-2xl overflow-hidden  border-4 border-gray-800">
-                <img className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-150 transition-transform duration-500 ease-in-out rounded-xl" src={SBCampus} alt="Image Description"/>
-               
-              </div>
-
-              <div className="mt-7">
-                <h3 className="text-xl font-semibold text-center text-gray-600 group-hover:bg-clip-text group-hover:bg-gradient-to-tr from-blue-600 to-purple-400 group-hover:text-transparent group-hover:scale-150 transition-transform duration-500 ease-in-out ">
-                 SAN BARTOLOME
-                </h3>
-              </div>
-            </a>
-            <a className="group rounded-2xl overflow-hidden hidden md:block" onClick={ClickBatasan}>
-              <div className="relative pt-[50%] sm:pt-[70%] rounded-2xl overflow-hidden  border-4 border-gray-800">
-                <img className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-150 transition-transform duration-500 ease-in-out rounded-xl" src={BatasanCampus} alt="Image Description"/>
-               
-              </div>
-
-              <div className="mt-7">
-                <h3 className="text-xl font-semibold text-center text-gray-600 group-hover:bg-clip-text group-hover:bg-gradient-to-tr from-blue-600 to-purple-400 group-hover:text-transparent group-hover:scale-150 transition-transform duration-500 ease-in-out ">
-                 BATASAN
-                </h3>
-              </div>
-            </a>
-
-            <a className="group rounded-2xl overflow-hidden hidden md:block " onClick={ClickSF}>
-              <div className="relative pt-[50%] sm:pt-[70%] rounded-2xl overflow-hidden  border-4 border-gray-800">
+          <div className="grid sm:grid-cols-2 mt-20 lg:grid-cols-3 gap-1 md:gap-6">
+            <a className="group rounded-3xl overflow-hidden hidden md:block " onClick={ClickSF}>
+              <div className="relative pt-[50%] sm:pt-[70%] rounded-3xl overflow-hidden ">
                 <img className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-150 transition-transform duration-500 ease-in-out rounded-xl" src={SFCampus} alt="Image Description" />
-                <span className="absolute top-0 right-0 group-hover:scale-150 transition-transform duration-500 ease-in-out rounded-bl-xl text-xs font-medium bg-gray-800 text-white py-1.5 px-3 dark:bg-gray-900">
-                  Soon
-                </span>
+
               </div>
 
               <div className="mt-7">
-                <h3 className="text-xl font-semibold text-center text-gray-600  group-hover:bg-clip-text group-hover:bg-gradient-to-tr from-blue-600 to-purple-400 group-hover:text-transparent group-hover:scale-150 transition-transform duration-500 ease-in-out">
-                 SAN FRANSISCO
+                <h3 className="text-xl font-semibold text-center text-gray-100  group-hover:bg-clip-text group-hover:bg-gradient-to-tr from-blue-600 to-purple-400 group-hover:text-transparent group-hover:scale-150 transition-transform duration-500 ease-in-out">
+                  SAN FRANSISCO
+                </h3>
+              </div>
+            </a>
+
+            <a className="group rounded-3xl overflow-hidden hidden md:block" onClick={ClickSB}>
+              <div className="relative pt-[50%] sm:pt-[70%] rounded-3xl overflow-hidden">
+                <img className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-150 transition-transform duration-500 ease-in-out rounded-xl" src={SBCampus} alt="Image Description" />
+
+              </div>
+
+              <div className="mt-7">
+                <h3 className="text-xl font-semibold text-center text-gray-100 group-hover:bg-clip-text group-hover:bg-gradient-to-tr from-blue-600 to-purple-400 group-hover:text-transparent group-hover:scale-150 transition-transform duration-500 ease-in-out ">
+                  SAN BARTOLOME
+                </h3>
+              </div>
+            </a>
+            <a className="group rounded-3xl overflow-hidden hidden md:block" onClick={ClickBatasan}>
+              <div className="relative pt-[50%] sm:pt-[70%] rounded-3xl overflow-hidden">
+                <img className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-150 transition-transform duration-500 ease-in-out rounded-xl" src={BatasanCampus} alt="Image Description" />
+
+              </div>
+
+              <div className="mt-7">
+                <h3 className="text-xl font-semibold text-center text-gray-100 group-hover:bg-clip-text group-hover:bg-gradient-to-tr from-blue-600 to-purple-400 group-hover:text-transparent group-hover:scale-150 transition-transform duration-500 ease-in-out ">
+                  BATASAN
                 </h3>
               </div>
             </a>
 
 
 
-<div className="max-w-[85rem] px-4 pb-10 sm:px-6 lg:px-8 lg:py-14 mx-auto md:hidden">
 
-  <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 sm:gap-6">
-   
-    <a className="group flex flex-col border-4 shadow-sm rounded-2xl hover:shadow-md transition border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" onClick={ClickSB}>
-      <div className="p-3 md:p-5">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <img className="h-20 w-20 rounded-2xl" src={SBCampus} alt="Image Description"/>
-            <div className="ms-3">
-              <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-400">
-                San Bartolome
-              </h3>
+            <div className="max-w-[85rem] px-4 pb-10 -mt-10 sm:px-6 lg:px-8 lg:py-14 mx-auto md:hidden">
+
+              <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 sm:gap-6">
+
+                <a className="group flex flex-col border-2 shadow-sm rounded-3xl hover:shadow-md transition border-t-2 dark:border-slate-600/60 backdrop-blur-lg dark:bg-slate-800/50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" onClick={ClickSB}>
+                  <div className="p-3 md:p-5">
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center">
+                        <img className="h-20 w-20 rounded-3xl" src={SBCampus} alt="Image Description" />
+                        <div className="ms-3">
+                          <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-400">
+                            San Bartolome
+                          </h3>
+                        </div>
+                      </div>
+                      <div className="ps-3">
+                        <Icon icon="fluent:caret-right-24-filled" className="w-8 h-8" />
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+
+
+                <a className="group flex flex-col border-2 shadow-sm rounded-3xl hover:shadow-md transition border-t-2 dark:border-slate-600/60 backdrop-blur-lg dark:bg-slate-800/50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" onClick={ClickBatasan}>
+                  <div className="p-3 md:p-5">
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center">
+                        <img className="h-20 w-20 rounded-3xl" src={BatasanCampus} alt="Image Description" />
+                        <div className="ms-3">
+                          <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-400">
+                            Batasan
+                          </h3>
+                        </div>
+                      </div>
+                      <div className="ps-3">
+                        <Icon icon="fluent:caret-right-24-filled" className="w-8 h-8" />
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+
+
+                <a className="group flex flex-col border-2 shadow-sm rounded-3xl hover:shadow-md transition border-t-2 dark:border-slate-600/60 backdrop-blur-lg dark:bg-slate-800/50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" onClick={ClickSF}>
+                  <div className="p-3 md:p-5">
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center">
+                        <img className="h-20 w-20 rounded-3xl" src={SFCampus} alt="Image Description" />
+                        <div className="ms-3">
+                          <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-400">
+                            San Fransisco
+                          </h3>
+                        </div>
+                      </div>
+                      <div className="ps-3">
+                        <Icon icon="fluent:caret-right-24-filled" className="w-8 h-8" />
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+
+
+              </div>
+
             </div>
-          </div>
-          <div className="ps-3">
-            <svg className="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
-          </div>
-        </div>
-      </div>
-    </a>
-  
-
-   
-    <a className="group flex flex-col border-4 shadow-sm rounded-2xl hover:shadow-md transition border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" onClick={ClickBatasan}>
-      <div className="p-3 md:p-5">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <img className="h-20 w-20 rounded-2xl" src={BatasanCampus} alt="Image Description"/>
-            <div className="ms-3">
-              <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-400">
-                Batasan
-              </h3>
-            </div>
-          </div>
-          <div className="ps-3">
-            <svg className="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
-          </div>
-        </div>
-      </div>
-    </a>
-  
-
-   
-    <a className="group flex flex-col border-4 shadow-sm rounded-2xl hover:shadow-md transition border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" onClick={ClickSF}>
-      <div className="p-3 md:p-5">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <img className="h-20 w-20 rounded-2xl" src={SFCampus} alt="Image Description"/>
-            <div className="ms-3">
-              <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-400">
-                San Fransisco
-              </h3>
-            </div>
-          </div>
-          <div className="ps-3">
-            <svg className="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
-          </div>
-        </div>
-      </div>
-    </a>
-  
-
-   
-  </div>
-
-</div>
 
           </div>
 
