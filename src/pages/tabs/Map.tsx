9040582860bc2682207e76fe537fceb9e4b540eb
Mike Gester <mikegester.sabuga023@gmail.com>
2023-11-29 06:24:26 +0800

    Added Sidemenu

--- src/pages/tabs/Map.tsx
+++ src/pages/tabs/Map.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonPage } from '@ionic/react';
 import SanBartolome from '../components/campus/SanBartolome';
 import Dock from '../components/dock';
+import { Icon } from '@iconify/react';
 import Controls from '../components/controls';
 import { IonButton, IonHeader, IonMenu, IonMenuToggle, IonTitle, IonToolbar } from '@ionic/react';
 //import Directions from './../components/directions';
@@ -12,6 +13,7 @@ interface ContainerProps {
 
 const Map: React.FC<ContainerProps> = ({ name }) => {
   const [showModal, setShowModal] = useState(false);
+
 
   const openModal = () => {
     setShowModal(true);
@@ -42,6 +44,8 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                 {/* <div className="bg-sky-900 w-20 h-60 m-2 rounded-2xl text-center justify-center py-16 px-auto text-xl backdrop-blur-lg ">1</div> */}
                 <div className="flex flex-col w-full">
                   <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('select_floor').showModal()}>
+                  <h3 className="text-gray-900 font-extrabold text-4xl">Ground</h3>
+
                     <dialog id="select_floor" className="modal">
                       <div className="modal-box  max-w-3xl">
                         <form method="dialog">
@@ -52,11 +56,144 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                         <h3 className="font-bold text-center text-3xl">Ground Floor</h3>
                         <div className="grid grid-cols-4 mt-10 gap-1">
                           <div className="flex flex-col w-full">
-                            
-                            <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+
+                            <div className="grid h-20 shadow-lg bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                              <h3 className="text-gray-900 font-extrabold text-4xl">IC101</h3>
                               <dialog id="my_modal_2" className="modal">
-                                <div className="modal-box max-w-3xl h-96 p-5">
-                                <IonButton onClick={openModal}>Open</IonButton>
+                                <div className="modal-box w-96 absolute right-0 top-0 transition-transform transform translate-x-0 ease-in-out duration-300  drawer-overlay  min-h-screen rounded-r-none p-5 ">
+                                  <h3 className="font-bold text-center text-3xl">Details</h3>
+
+                                  <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-0 lg:py-5 mx-auto">
+
+                                    <div class="grid md:grid-cols-1 border border-gray-200 shadow-sm rounded-xl overflow-hidden dark:border-gray-700">
+                                      <h3 className="font-bold text-center text-xl">IC101</h3>
+                                      <a class="block p-4 md:p-5  hover:bg-gray-50 before:absolute before:top-0 before:start-0 before:w-full before:h-px md:before:w-px md:before:h-full  before:first:bg-transparent  dark:hover:bg-slate-800 dark:before:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                        <div class="flex md:grid lg:flex gap-y-3 gap-x-5">
+                                          <Icon icon="mdi:information-variant-circle-outline" className="w-10 h-10 text-gray-500" />
+
+                                          <div class="grow">
+                                            <p class="text-xs uppercase tracking-wide font-medium text-gray-800 dark:text-gray-200">
+                                              Belmonte Building
+                                            </p>
+                                            <h3 class="mt-1 text-xl sm:text-2xl font-semibold text-gray-500">
+                                              2nd Floor
+                                            </h3>
+                                            <div class="mt-1 flex justify-between items-center">
+                                              <p class="text-sm text-gray-500">
+                                                Room: <span class="font-semibold text-gray-800 dark:text-gray-200">IC101</span>
+                                              </p>
+
+                                            </div>
+                                          </div>
+                                        </div>
+
+                                      </a>
+                                      <a class="block p-4 md:p-5  hover:bg-gray-50 before:absolute before:top-0 before:start-0 before:w-full before:h-px md:before:w-px md:before:h-full  before:first:bg-transparent  dark:hover:bg-slate-800 dark:before:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                        <div class="flex md:grid lg:flex gap-y-3 gap-x-5">
+                                          <Icon icon="pepicons-pop:rewind-time" className="w-10 h-10 text-gray-500" />
+
+                                          <div class="grow">
+                                            <p class="text-xs uppercase tracking-wide font-medium text-gray-800 dark:text-gray-200">
+                                              Estimated Time of Arrival ( ETA )
+                                            </p>
+                                            <h3 class="mt-1 text-xl sm:text-2xl font-semibold text-gray-500">
+                                              4:26
+                                            </h3>
+                                            <div class="mt-1 flex justify-between items-center">
+                                              <p class="text-sm text-gray-500">
+                                                Average: <span class="font-semibold text-gray-800 dark:text-gray-200">4:11</span>
+                                              </p>
+
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </a>
+                                      <hr className="bg-gray-400 m-6" />
+                                      <h3 className="font-bold text-center text-3xl">Directions</h3>
+                                      <a class="block p-4 md:p-5  hover:bg-gray-50 before:absolute before:top-0 before:start-0 before:w-full before:h-px md:before:w-px md:before:h-full  before:first:bg-transparent  dark:hover:bg-slate-800 dark:before:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                        <div class="flex md:grid lg:flex gap-y-3 gap-x-5">
+                                          <Icon icon="mdi:arrow-up-thick" className="w-10 h-10 text-gray-500" />
+
+                                          <div class="grow">
+                                            <p class="text-xs uppercase tracking-wide font-medium text-gray-800 dark:text-gray-200">
+                                              Head towards Techvoc Building
+                                            </p>
+
+                                            <div class="mt-1 flex justify-between items-center">
+                                              <p class="text-sm text-gray-500">
+                                                Average: <span class="font-semibold text-gray-800 dark:text-gray-200">4:11</span>
+                                              </p>
+
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </a>
+                                      <a class="block p-4 md:p-5  hover:bg-gray-50 before:absolute before:top-0 before:start-0 before:w-full before:h-px md:before:w-px md:before:h-full  before:first:bg-transparent  dark:hover:bg-slate-800 dark:before:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                        <div class="flex md:grid lg:flex gap-y-3 gap-x-5">
+                                          <Icon icon="mdi:arrow-left-top-bold" className="w-10 h-10 text-gray-500" />
+
+                                          <div class="grow">
+                                            <p class="text-xs uppercase tracking-wide font-medium text-gray-800 dark:text-gray-200">
+                                              Head towards Techvoc Building
+                                            </p>
+
+                                            <div class="mt-1 flex justify-between items-center">
+                                              <p class="text-sm text-gray-500">
+                                                Average: <span class="font-semibold text-gray-800 dark:text-gray-200">4:11</span>
+                                              </p>
+
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </a>
+                                      <a class="block p-4 md:p-5  hover:bg-gray-50 before:absolute before:top-0 before:start-0 before:w-full before:h-px md:before:w-px md:before:h-full  before:first:bg-transparent  dark:hover:bg-slate-800 dark:before:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                        <div class="flex md:grid lg:flex gap-y-3 gap-x-5">
+                                          <Icon icon="mdi:arrow-right-top-bold" className="w-10 h-10 text-gray-500" />
+
+                                          <div class="grow">
+                                            <p class="text-xs uppercase tracking-wide font-medium text-gray-800 dark:text-gray-200">
+                                              Head towards Techvoc Building
+                                            </p>
+
+                                            <div class="mt-1 flex justify-between items-center">
+                                              <p class="text-sm text-gray-500">
+                                                Average: <span class="font-semibold text-gray-800 dark:text-gray-200">4:11</span>
+                                              </p>
+
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </a>
+                                      <a class="block p-4 md:p-5  hover:bg-gray-50 before:absolute before:top-0 before:start-0 before:w-full before:h-px md:before:w-px md:before:h-full  before:first:bg-transparent  dark:hover:bg-slate-800 dark:before:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                        <div class="flex md:grid lg:flex gap-y-3 gap-x-5">
+                                          <Icon icon="mdi:arrow-up-thick" className="w-10 h-10 text-gray-500" />
+
+                                          <div class="grow">
+                                            <p class="text-xs uppercase tracking-wide font-medium text-gray-800 dark:text-gray-200">
+                                              Head towards Techvoc Building
+                                            </p>
+
+                                            <div class="mt-1 flex justify-between items-center">
+                                              <p class="text-sm text-gray-500">
+                                                Average: <span class="font-semibold text-gray-800 dark:text-gray-200">4:11</span>
+                                              </p>
+
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </a>
+
+
+                                    </div>
+
+                                    <div className="absolute bottom-5 inset-x-0 mx-6">                                      
+                                      <button className="btn w-full bg-sky-800 rounded-xl">
+                                      <Icon icon="material-symbols:moved-location-rounded" className="w-8 h-8" />
+                                        Start Navigation
+                                      </button>
+                                    </div>
+                                  </div>
+
                                 </div>
                                 <form method="dialog" className="modal-backdrop">
                                   <button>close</button>
@@ -66,6 +203,7 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                           </div>
                           <div className="flex flex-col w-full">
                             <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                            <h3 className="text-gray-900 font-extrabold text-4xl">IC102</h3>
                               <dialog id="my_modal_2" className="modal">
                                 <div className="modal-box max-w-3xl">
                                   <h3 className="font-bold text-lg">Hello!</h3>
@@ -79,6 +217,8 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                           </div>
                           <div className="flex flex-col w-full">
                             <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                            <h3 className="text-gray-900 font-extrabold text-4xl">IC103</h3>
+
                               <dialog id="my_modal_2" className="modal">
                                 <div className="modal-box max-w-3xl">
                                   <h3 className="font-bold text-lg">Hello!</h3>
@@ -92,6 +232,8 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                           </div>
                           <div className="flex flex-col w-full">
                             <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                            <h3 className="text-gray-900 font-extrabold text-4xl">IC104</h3>
+
                               <dialog id="my_modal_2" className="modal">
                                 <div className="modal-box max-w-3xl">
                                   <h3 className="font-bold text-center text-5xl">Select Room</h3>
@@ -111,6 +253,8 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                           </div>
                           <div className="flex flex-col w-full">
                             <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                            <h3 className="text-gray-900 font-extrabold text-4xl">IC105</h3>
+
                               <dialog id="my_modal_2" className="modal">
                                 <div className="modal-box max-w-3xl">
                                   <h3 className="font-bold text-lg">Hello!</h3>
@@ -126,6 +270,7 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                           </div>
                           <div className="flex flex-col w-full">
                             <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                            <h3 className="text-gray-900 font-extrabold text-4xl">IC106</h3>
                               <dialog id="my_modal_2" className="modal">
                                 <div className="modal-box max-w-3xl">
                                   <h3 className="font-bold text-lg">Hello!</h3>
@@ -141,6 +286,7 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                           </div>
                           <div className="flex flex-col w-full">
                             <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                            <h3 className="text-gray-900 font-extrabold text-4xl">IC107</h3>
                               <dialog id="my_modal_2" className="modal">
                                 <div className="modal-box max-w-3xl">
                                   <h3 className="font-bold text-center text-5xl">Select Room</h3>
@@ -160,6 +306,7 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                           </div>
                           <div className="flex flex-col w-full">
                             <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                            <h3 className="text-gray-900 font-extrabold text-4xl">IC108</h3>
                               <dialog id="my_modal_2" className="modal">
                                 <div className="modal-box max-w-3xl">
                                   <h3 className="font-bold text-lg">Hello!</h3>
@@ -183,6 +330,7 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                 </div>
                 <div className="flex flex-col w-full">
                   <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                  <h3 className="text-gray-900 font-extrabold text-4xl">2nd</h3>
                     <dialog id="my_modal_2" className="modal">
                       <div className="modal-box max-w-3xl">
                         <h3 className="font-bold text-center text-5xl">Select Room</h3>
@@ -202,6 +350,7 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                 </div>
                 <div className="flex flex-col w-full">
                   <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                  <h3 className="text-gray-900 font-extrabold text-4xl">3rd</h3>
                     <dialog id="my_modal_2" className="modal">
                       <div className="modal-box max-w-3xl">
                         <h3 className="font-bold text-lg">Hello!</h3>
@@ -217,6 +366,7 @@ const Map: React.FC<ContainerProps> = ({ name }) => {
                 </div>
                 <div className="flex flex-col w-full">
                   <div className="grid h-20 card bg-white hover:scale-75 ease-in-out duration-200 rounded-box place-items-center" onClick={() => document.getElementById('my_modal_2').showModal()}>
+                  <h3 className="text-gray-900 font-extrabold text-4xl">4th</h3>
                     <dialog id="my_modal_2" className="modal">
                       <div className="modal-box max-w-3xl">
                         <h3 className="font-bold text-lg">Hello!</h3>
