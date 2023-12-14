e91e9e7060d6d97c151d17cf11271c5e51848ecd
Mike Gester <mikegester.sabuga023@gmail.com>
2023-12-14 20:31:04 +0800

    statusbar mode for small screen and mobile

---
+++ src/pages/components/Mobile/StatusBar.tsx
@@ -0,0 +1,12 @@
+import { Icon } from '@iconify/react';
+
+
+const StatusBar: React.FC = () => {
+
+  return (
+    <div className="fixed top md:hidden h-12 inline-flex mx-auto justify-between border-t-2 dark:border-slate-600/60 backdrop-blur-lg dark:bg-slate-800/50 w-full sm:w-5/12 md:w-4/12 lg:w-3/12 xl:w-2/12 rounded-b-2xl">
+    </div>
+  );
+};
+
+export default StatusBar;
