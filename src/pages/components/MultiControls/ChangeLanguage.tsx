2a96866d3bb4e219a2ddacf135d96433051ce6f1
Mike Gester <mikegester.sabuga023@gmail.com>
2023-12-14 20:29:58 +0800

    Hide some of the 10 languages

--- src/pages/components/MultiControls/ChangeLanguage.tsx
+++ src/pages/components/MultiControls/ChangeLanguage.tsx
@@ -43,8 +43,8 @@ const ChangeLanguage: React.FC<ContainerProps> = ({ name }) => {
                 <div className="dropdown dropdown-right dropdown-end ">
                     <label tabIndex={0} className=" m-1"><Icon icon="ion:language" className="w-5 h-5" /></label>
 
-                    <ul tabIndex={0} className=" grid grid-cols-2 gap-1 dropdown-content border-t bg-slate-800/90 border-slate-600/60 backdrop-blur-3xl z-[1] menu p-2 ml-7 shadow-lg rounded-2xl w-96">
-                        <h1 className="col-span-2 text-center" >{t("SelectLanguage")}</h1>
+                    <ul tabIndex={0} className=" grid grid-cols-1 gap-1 dropdown-content border-t bg-slate-800/90 border-slate-600/60 backdrop-blur-lg z-[1] menu p-2 ml-7 shadow-lg rounded-2xl w-64">
+                        <h1 className="col-span-1 text-center" >{t("SelectLanguage")}</h1>
                         <li>
                             <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['en']}`} onClick={(e) => handleLanguageChange(e, 'en')}>
                                 English {' '}
@@ -58,18 +58,19 @@ const ChangeLanguage: React.FC<ContainerProps> = ({ name }) => {
                             </a>
                         </li>
                         <li>
-                            <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['sp']}`} onClick={(e) => handleLanguageChange(e, 'sp')}>
-                               Spanish {' '}
-                                <Icon icon="emojione-v1:flag-for-spain" className="w-10 h-10" />
-                            </a>
-                        </li>
-                        <li>
                             <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['kr']}`} onClick={(e) => handleLanguageChange(e, 'kr')}>
                                 Korean {' '}
                                 <Icon icon="emojione-v1:flag-for-south-korea" className="w-10 h-10" />
                             </a>
                         </li>
-                        <li>
+                        {/* <li>
+                            <li>
+                                <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['sp']}`} onClick={(e) => handleLanguageChange(e, 'sp')}>
+                                    Spanish {' '}
+                                    <Icon icon="emojione-v1:flag-for-spain" className="w-10 h-10" />
+                                </a>
+                            </li>
+
                             <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['jpn']}`} onClick={(e) => handleLanguageChange(e, 'jpn')}>
                                 Japanese {' '}
                                 <Icon icon="emojione-v1:flag-for-japan" className="w-10 h-10" />
@@ -82,29 +83,29 @@ const ChangeLanguage: React.FC<ContainerProps> = ({ name }) => {
                             </a>
                         </li>
                         <li>
-                        <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['vn']}`} onClick={(e) => handleLanguageChange(e, 'vn')}>
+                            <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['vn']}`} onClick={(e) => handleLanguageChange(e, 'vn')}>
                                 Vietnamese{' '}
                                 <Icon icon="emojione-v1:flag-for-vietnam" className="w-10 h-10" />
                             </a>
                         </li>
                         <li>
-                        <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['fr']}`} onClick={(e) => handleLanguageChange(e, 'fr')}>
+                            <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['fr']}`} onClick={(e) => handleLanguageChange(e, 'fr')}>
                                 French {' '}
                                 <Icon icon="emojione-v1:flag-for-france" className="w-10 h-10" />
                             </a>
                         </li>
                         <li>
-                        <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['cmd']}`} onClick={(e) => handleLanguageChange(e, 'cmd')}>
+                            <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['cmd']}`} onClick={(e) => handleLanguageChange(e, 'cmd')}>
                                 Khmer{' '}
                                 <Icon icon="emojione-v1:flag-for-cambodia" className="w-10 h-10" />
                             </a>
                         </li>
                         <li>
-                        <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['thai']}`} onClick={(e) => handleLanguageChange(e, 'thai')}>
+                            <a href="#" className={`justify-between border-2 border-transparent text-white ${borderStyles['thai']}`} onClick={(e) => handleLanguageChange(e, 'thai')}>
                                 Thai {' '}
                                 <Icon icon="emojione-v1:flag-for-thailand" className="w-10 h-10" />
                             </a>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
             </div>
