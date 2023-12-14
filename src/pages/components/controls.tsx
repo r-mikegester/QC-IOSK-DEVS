bfeea9006016621688e49c73353e2187d78eca60
Mike Gester <mikegester.sabuga023@gmail.com>
2023-12-14 20:32:05 +0800

    removed unnecessary component and fix bugs

--- src/pages/components/controls.tsx
+++ src/pages/components/controls.tsx
@@ -1,13 +1,10 @@
 import { Icon } from '@iconify/react';
-import React from 'react';
-import ChangeLanguageModal from './MultiControls/Credits';
-import AudioBg from '../Components/MultiControls/AudioBG';
+import React, { useState } from 'react';
 import ChangeLanguage from './MultiControls/ChangeLanguage';
+import Credits from './MultiControls/Credits';
 import KioskManual from './MultiControls/KioskManual';
 import Reload from './MultiControls/Reload';
-import Credits from './MultiControls/Credits';
-import Modal from './Modals/CreditsModal';
-import SelectFloorModal from './Modals/SelectFloorModal';
+import AudioBG from './MultiControls/AudioBG';
 interface ContainerProps {
     name: string;
 }
@@ -16,7 +13,6 @@ const Controls: React.FC<ContainerProps> = ({ name }) => {
     const reloadPage = () => {
         window.location.reload();
     };
-
 
     return (
         <div className=" w-10 z-50">
@@ -27,15 +23,15 @@ const Controls: React.FC<ContainerProps> = ({ name }) => {
                     className="z-20 flex shrink-0 grow-0 justify-around border-t border-gray-200 bg-white/50 p-3 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-96 mt-52 md:mt-0 md:top-3/4 -translate-y-2/4 left-6 h-auto gap-2 min-w-[64px] flex-col rounded-2xl border"
                 >
                     <KioskManual  name={'KioskManual'} />
-                    <AudioBg name={'AudioBG'} />
+                    <AudioBG name={'AudioBG'} />
                     <Reload  name={'Reload'} />
                     
                     <hr className=" bg-sky-400 my-2" />
                    
                     <ChangeLanguage name={'ChangeLanguage'} />
                   
-                    <Credits />
-                   <SelectFloorModal />
+                    <Credits name={'credits'} />
+                  
                 </nav>
 
             </div>
