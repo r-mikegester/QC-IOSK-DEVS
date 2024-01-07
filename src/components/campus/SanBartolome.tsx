13c7028cab04f075239bc125c2d9031a7b26d19e
Mike Gester <mikegester.sabuga023@gmail.com>
2024-01-08 06:22:36 +0800

    New Map Controls

--- src/components/campus/SanBartolome.tsx
+++ src/components/campus/SanBartolome.tsx
@@ -1,6 +1,29 @@
-import { OrbitControls, Stage, Bounds, useBounds, Html, Text, Billboard, Sparkles, Stars } from "@react-three/drei";
-import { Object3D, Object3DEventMap, Box3, Material, NormalBufferAttributes, BufferGeometry, Mesh } from "three";
-import { Selection, EffectComposer, Outline, Select } from "@react-three/postprocessing";
+import {
+  OrbitControls,
+  Stage,
+  Bounds,
+  useBounds,
+  Html,
+  Text,
+  Billboard,
+  Sparkles,
+  Stars,
+} from "@react-three/drei";
+import {
+  Object3D,
+  Object3DEventMap,
+  Box3,
+  Material,
+  NormalBufferAttributes,
+  BufferGeometry,
+  Mesh,
+} from "three";
+import {
+  Selection,
+  EffectComposer,
+  Outline,
+  Select,
+} from "@react-three/postprocessing";
 import React, { Suspense, useRef, useState, ReactNode, useEffect } from "react";
 import { Canvas, useLoader, useThree, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
@@ -9,6 +32,8 @@ import * as TWEEN from "@tweenjs/tween.js";
 import { Icon } from "@iconify/react";
 import { format } from "date-fns";
 import * as THREE from "three";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 interface ModelProps {
   url: string;
   scale: number;
@@ -118,18 +143,24 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
   const [isColor1, setIsColor1] = useState(true);
   const [isEnvironment, setIsEnvironment] = useState(true);
   const [isLoading, setIsLoading] = useState(false); // Loading state
-
-  const handleToggleSwitch = () => {
-    setIsColor1((prev) => !prev);
-    setIsEnvironment((prev) => !prev);
+  const [autoRotate, setAutoRotate] = useState(true); // Initial state set to true for autorotation
+  const [isEnvironmentTwo, setIsEnvironmentTwo] = useState(false);
+  const [isNightMode, setIsNightMode] = useState(true);
+  const [isCloudOn, setCloudOn] = useState(false);
+  const [isStars, setStars] = useState(false);
+  const clearWaitingQueue = () => {
+    // Easy, right 😎
+    toast.clearWaitingQueue();
   };
+
+  // Display a toast message when thins clicked is changed
 
   // Function to check if the current time is between 6 PM and 4 AM
   const isNightTime = () => {
     const currentDate = new Date();
     const currentHour = parseInt(format(currentDate, "H"), 10); // Extract the current hour (0-23)
 
-    return currentHour >=  18 || currentHour < 4; // 18 is for Nighttime and 4 is for daytime 
+    return currentHour >= 18 || currentHour < 4; // 18 is for Nighttime and 4 is for daytime
   };
 
   // Function to handle the time-based toggle
@@ -141,7 +172,8 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
       // Simulate a delay for the transition to night mode using setTimeout
       setTimeout(() => {
         setIsColor1(false); // Set color 2 when in the specified time range
-        setIsEnvironment(false); // Modify environment settings as needed
+        setIsEnvironment(false);
+        // setIsEnvironmentTwo(false); // Modify environment settings as needed
         setIsLoading(false);
         // console.log('Night Mode Activated'); // Set loading state to false after transitioning
       }, 0); // Adjust the delay time as needed (in milliseconds)
@@ -151,7 +183,8 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
       // Simulate a delay for the transition back to day mode using setTimeout
       setTimeout(() => {
         setIsColor1(true); // Set color 1 when outside the specified time range
-        setIsEnvironment(true); // Modify environment settings as needed
+        setIsEnvironment(true);
+        // setIsEnvironmentTwo(true); // Modify environment settings as needed
         setIsLoading(false);
         // console.log("Day Mode Activated"); // Set loading state to false after transitioning
       }, 0); // Adjust the delay time as needed (in milliseconds)
@@ -169,12 +202,80 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
 
   return (
     <>
-      {/* <button
-        onClick={handleToggleSwitch}
-        className="absolute  bottom-8 backdrop-blur-lg bg-base-100 text-base-content right-5 z-50 flex aspect-square min-h-[32px] w-12 flex-col items-center scale-125 gap-1 justify-center rounded-xl border border-gray-800 hover:bg-base-300 hover:scale-105 duration-200 ease-in-out"
-      >
-        <Icon icon="icon-park-outline:dark-mode" className="w-6 h-6" />
-      </button> */}
+      <div className="join absolute top-5 right-96">
+        <button
+          onClick={() => setAutoRotate(!autoRotate)}
+          className="tooltip tooltip-bottom join-item bg-base-100 text-base-content z-50 flex p-2 flex-col items-center gap-1 justify-center rounded-xl border border-gray-800 hover:bg-base-300 duration-200 ease-in-out"
+          data-tip={t(autoRotate ? "Stop Rotation" : "Start Rotation")}
+        >
+          {/* {autoRotate ? "Stop Rotation" : "Start Rotation"} */}
+          {autoRotate ? (
+            <Icon
+              icon="gravity-ui:arrows-rotate-right-slash"
+              className="w-8 h-8"
+            />
+          ) : (
+            <Icon icon="gravity-ui:arrows-rotate-right" className="w-8 h-8" />
+          )}
+        </button>
+        <button
+          onClick={() => setIsEnvironmentTwo(!isEnvironmentTwo)}
+          className="tooltip tooltip-bottom join-item bg-base-100 text-base-content z-50 flex p-2 flex-col items-center gap-1 justify-center rounded-xl border border-gray-800 hover:bg-base-300 duration-200 ease-in-out"
+          data-tip={t(isEnvironmentTwo ? "Lights On" : "Lights Off")}
+        >
+          {isEnvironmentTwo ? (
+            <Icon icon="line-md:lightbulb" className="w-8 h-8" />
+          ) : (
+            <Icon icon="line-md:lightbulb-off" className="w-8 h-8" />
+          )}
+        </button>
+        <button
+          name="cloud"
+          onClick={() => setCloudOn(!isCloudOn)}
+          className="tooltip tooltip-bottom join-item bg-base-100 text-base-content z-50 flex p-2 flex-col items-center gap-1 justify-center rounded-xl border border-gray-800 hover:bg-base-300 duration-200 ease-in-out"
+          data-tip={t(isCloudOn ? "Clouds On" : "Clouds Off")}
+        >
+          {/* {autoRotate ? "Stop Rotation" : "Start Rotation"} */}
+          {isCloudOn ? (
+            <Icon icon="tabler:cloud" className="w-8 h-8" />
+          ) : (
+            <Icon icon="tabler:cloud-off" className="w-8 h-8" />
+          )}
+        </button>
+        <button
+          name="nightmodeswitch"
+          onClick={() => setIsNightMode(!isNightMode)}
+          className="tooltip tooltip-bottom join-item bg-base-100 text-base-content z-50 flex p-2 flex-col items-center gap-1 justify-center rounded-xl border border-gray-800 hover:bg-base-300 duration-200 ease-in-out"
+          data-tip={t(isNightMode ? "Night Mode" : "Day Mode")}
+        >
+          {/* {autoRotate ? "Stop Rotation" : "Start Rotation"} */}
+          {isNightMode ? (
+            <Icon
+              icon="material-symbols:nights-stay-outline-rounded"
+              className="w-8 h-8"
+            />
+          ) : (
+            <Icon
+              icon="material-symbols:partly-cloudy-day-outline-rounded"
+              className="w-8 h-8"
+            />
+          )}
+        </button>
+        
+        <button
+          name="stars"
+          onClick={() => setStars(!isStars)}
+          className="tooltip tooltip-bottom join-item bg-base-100 text-base-content z-50 flex p-2 flex-col items-center gap-1 justify-center rounded-xl border border-gray-800 hover:bg-base-300 duration-200 ease-in-out"
+          data-tip={t(isStars ? "Stars On" : "Stars Off")}
+        >
+          {/* {autoRotate ? "Stop Rotation" : "Start Rotation"} */}
+          {isStars ? (
+            <Icon icon="tabler:stars"  className="w-8 h-8" />
+          ) : (
+            <Icon icon="tabler:stars-off" className="w-8 h-8" />
+          )}
+        </button>
+      </div>
 
       {/* Conditionally render the loading spinner based on the isLoading state */}
       {isLoading && (
@@ -191,7 +292,7 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
         dpr={[1, 2]}
         camera={{ fov: 75, position: [10, 10, 10], zoom: 20 }}
         className={
-          isColor1
+          isNightMode
             ? "bg-gradient-to-tr from-sky-900 to-sky-400"
             : "bg-gradient-to-tr from-stone-950 to-cyan-950"
         }
@@ -206,57 +307,61 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
           enableZoom={true}
           enablePan={true}
           zoomSpeed={3}
-          autoRotate={true}
+          autoRotate={autoRotate} // Set autoRotate to the state value to toggle autorotation
           autoRotateSpeed={0.4}
           minZoom={5}
           maxZoom={60}
         />
 
         <Suspense fallback={null}>
-          <mesh position={[10, 20, -60]}>
-            <Model // BACK
-              url="/src/assets/Models/clouds/cloud.glb"
-              scale={1.5}
-              name={"cloud"}
-            />
-          </mesh>
-          <mesh position={[0, 20, 20]}>
-            <Model // LEFT
-              url="/src/assets/Models/clouds/cloud1.glb"
-              scale={1.2}
-              name={"cloud1"}
-            />
-          </mesh>
-          <mesh position={[40, 20, -30]}>
-            <Model // RIGHT
-              url="/src/assets/Models/clouds/cloud2.glb"
-              scale={1.3}
-              name={"cloud2"}
-            />
-          </mesh>
-          <mesh position={[-20, 20, 70]}>
-            <Model // FRONT
-              url="/src/assets/Models/clouds/cloud3.glb"
-              scale={1.2}
-              name={"cloud3"}
-            />
-          </mesh>
-          <mesh position={[30, 20, 50]}>
-            <Model // FRONT RIGHT
-              url="/src/assets/Models/clouds/cloud4.glb"
-              scale={1.4}
-              name={"cloud4"}
-            />
-          </mesh>
-          <mesh position={[-30, 20, -40]}>
-            <Model // BACK LEFT
-              url="/src/assets/Models/clouds/cloud5.glb"
-              scale={1}
-              name={"cloud5"}
-            />
-          </mesh>
+          {isCloudOn ? null : (
+          <>
+            <mesh position={[10, 20, -60]}>
+              <Model // BACK
+                url="/src/assets/Models/clouds/cloud.glb"
+                scale={1.5}
+                name={"cloud"}
+              />
+            </mesh>
+            <mesh position={[0, 20, 20]}>
+              <Model // LEFT
+                url="/src/assets/Models/clouds/cloud1.glb"
+                scale={1.2}
+                name={"cloud1"}
+              />
+            </mesh>
+            <mesh position={[40, 20, -30]}>
+              <Model // RIGHT
+                url="/src/assets/Models/clouds/cloud2.glb"
+                scale={1.3}
+                name={"cloud2"}
+              />
+            </mesh>
+            <mesh position={[-20, 20, 70]}>
+              <Model // FRONT
+                url="/src/assets/Models/clouds/cloud3.glb"
+                scale={1.2}
+                name={"cloud3"}
+              />
+            </mesh>
+            <mesh position={[30, 20, 50]}>
+              <Model // FRONT RIGHT
+                url="/src/assets/Models/clouds/cloud4.glb"
+                scale={1.4}
+                name={"cloud4"}
+              />
+            </mesh>
+            <mesh position={[-30, 20, -40]}>
+              <Model // BACK LEFT
+                url="/src/assets/Models/clouds/cloud5.glb"
+                scale={1}
+                name={"cloud5"}
+              />
+            </mesh>
+          </>
+          )}
           <Bounds fit clip observe margin={1.2}>
-            {isEnvironment ? null : (
+            {isNightMode ? null : (
               <>
                 {/* <directionalLight position={[0, 0, 10]} intensity={20} /> */}
                 {/* <hemisphereLight position={[0, 0, 10]} intensity={10} /> */}
@@ -268,13 +373,15 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
                     intensity={500}
                   />
                   <pointLight position={[10, 10, -60]} intensity={100} /> */}
-                <Stars
+                {isStars ? null : (
+                  <Stars
                   radius={40} // Adjust the radius based on your scene size
                   depth={50} // Adjust the depth based on your scene size
                   count={50} // Adjust the count based on the number of sparkles you want
                   factor={0} // Adjust the factor to control the distribution of sparkles
                   saturation={0} // Adjust the saturation to control the color of sparkles
                 />
+                )}
                 {/* <Sparkles
                     color="orange"
                     count={500}
@@ -287,14 +394,14 @@ const SanBartolome: React.FC<ContainerProps> = ({ name }) => {
               </>
             )}
             <Stage
-              environment={isEnvironment ? "city" : "night"}
+              environment={isNightMode ? "city" : "night"}
               adjustCamera
               shadows
             >
               {/* <ambientLight intensity={0.5} /> */}
               {/* <spotLight position={[10, 10, 100]} angle={0.15} penumbra={1} intensity={0} /> */}
               {/* <pointLight position={[-10, -10, -10]} /> */}
-              {isEnvironment ? null : (
+              {isEnvironmentTwo ? null : (
                 <>
                   {/* BELMONTE */}
                   <rectAreaLight
