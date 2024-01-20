c6a73ecebe9273a736bc63bdb82281747f00e007
Mike Gester <mikegester.sabuga023@gmail.com>
2024-01-21 05:36:53 +0800

    recentered dock

--- src/components/controls/navigationControls/dock.tsx
+++ src/components/controls/navigationControls/dock.tsx
@@ -24,15 +24,15 @@ const Dock: React.FC<ContainerProps> = ({ name }) => {
 
   const { t } = useTranslation();
   return (
-    
-    <div className="fixed bottom-0 w-auto menu">
-      <div className="w-auto h-20 my-2 shadow-md bg-base-100/50 backdrop-blur-lg rounded-2xl">
+
+    <div className="fixed bottom-0 w-screen">
+      <div className="w-fit h-20 my-2 flex mx-auto shadow-md bg-base-100/50 backdrop-blur-lg rounded-2xl">
         <div className="px-2 py-2 space-x-1">
           <div className="tooltip " data-tip={t("Home")}>
             <a onClick={ClickWelcome} className="">
               <div className="w-16 h-16 duration-150 ease-in-out border border-none group btn bg-gradient-to-tr rounded-xl hover:-translate-y-5 from-base-100 to-base-300">
                 <Icon icon="octicon:home-16" className="w-10 h-10 " />
-                {/* <span className="-mt-5 text-xs duration-150 ease-in-out group-hover:mt-1 -z-50">{t("home")}</span> */}
+
               </div>
             </a>
           </div>
@@ -45,7 +45,7 @@ const Dock: React.FC<ContainerProps> = ({ name }) => {
                 icon="carbon:ibm-data-product-exchange"
                 className="w-10 h-10 "
               />
-              {/* <span className="-mt-5 text-xs duration-150 ease-in-out group-hover:mt-1 -z-50">Maps</span> */}
+
             </a>
           </div>
           <div className="tooltip " data-tip={t("Search")}>
@@ -54,15 +54,12 @@ const Dock: React.FC<ContainerProps> = ({ name }) => {
               className="w-16 h-16 duration-150 ease-in-out border-none btn group hover:-translate-y-5 bg-gradient-to-tr rounded-xl from-base-100 to-base-300"
             >
               <Icon icon="wpf:search" className="w-10 h-10 " />
-              {/* <span className="-mt-5 text-xs duration-150 ease-in-out group-hover:mt-1 -z-50">{t("search")}</span> */}
+
             </a>
           </div>
-          {/* <a className="w-16 h-16 duration-150 ease-in-out border-none btn group hover:-translate-y-5 bg-gradient-to-tr rounded-xl from-base-100 to-base-300">
-            <Icon icon="octicon:home-16" className="w-10 h-10 " />
-            <span className="-mt-5 text-xs duration-150 ease-in-out group-hover:mt-1 -z-50">Others</span>
-          </a> */}
+
         </div>
-        {/* <div className="w-auto h-10 bg-sky-700 rounded-xl"></div> */}
+
       </div>
     </div>
   );
