aecc32bf064a1be10337eda690152300ea9b09d7
Mike Gester <mikegester.sabuga023@gmail.com>
2024-03-22 06:33:56 +0800

    added batasan and sanfrancisco

--- src/components/campus/batasan/Batasan.tsx
+++ src/components/campus/batasan/Batasan.tsx
@@ -1,13 +1,18 @@
+import SideBar from "../../sidebar/sidebarLayout";
+
 interface ContainerProps {
   name: string;
 }
 
 const Batasan: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container mx-auto ">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">more campus later</a></p>
-    </div>
+    <>
+      <SideBar />
+      <div className="container mx-auto text-base-content">
+        <strong className="text-9xl">{name}</strong>
+        <p>Explore <a target="_blank" rel="noopener noreferrer" href="/">more campus later</a></p>
+      </div>
+    </>
   );
 };
 
--- src/components/campus/sanFrancisco/SanFrancisco.tsx
+++ src/components/campus/sanFrancisco/SanFrancisco.tsx
@@ -1,13 +1,18 @@
+import SideBar from "../../sidebar/sidebarLayout";
+
 interface ContainerProps {
   name: string;
 }
 
 const SanFransisco: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container mx-auto ">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">more campus later</a></p>
+    <>
+    <SideBar />
+    <div className="container mx-auto text-base-content">
+      <strong className="text-9xl">{name}</strong>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="/">more campus later</a></p>
     </div>
+    </>
   );
 };
 
